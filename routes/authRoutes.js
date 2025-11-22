9171ba053f5d8b5d1af0a72a598cf851824cca51
karl a <supersimple.deva@.com>
2025-11-22 19:12:47 +0100

    backend igje bruh

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { auth as authMiddleware } from "../middleware/authMiddleware.js";
 import { register, login, getMe } from "../controllers/authController.js";
-import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+// Test
+router.get("/ping", (req, res) => {
+  res.json({ message: "Auth API is working" });
+});
+
+// Protected route
+router.get("/me", authMiddleware, getMe);
+
+// Auth routes
 router.post("/register", register);
 router.post("/login", login);
-router.get("/me", authMiddleware, getMe);
 
 export default router;
